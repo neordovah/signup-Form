a15d43a012f24ac412d4fc92ac65ed09212592f5
ruxandra <pepicoal@yahoo.com>
2022-06-28 19:19:23 +0300

    added js for green and red input borders

---
+++ javascript.js
@@ -0,0 +1,38 @@
+const password = document.getElementById("password");
+const c_password = document.getElementById("c_password");
+const body = document.getElementById("body");
+const submit = document.getElementById("submit");
+
+function removeClasses() {
+    c_password.classList.remove("green");
+    c_password.classList.remove("red");
+}
+
+function check() {
+    if (password.value === c_password.value && password.value != "") {
+      c_password.setCustomValidity('');
+      c_password.classList.remove("red");
+      c_password.classList.add("green");
+    }
+    else if (password.value === '')
+    {
+        removeClasses();
+    }
+    else if (password.value != c_password.value && password.value != ""){
+      c_password.setCustomValidity('Passwords do not match');
+      c_password.classList.remove("green");
+      c_password.classList.add("red");
+    }
+}
+
+body.addEventListener('change', () => {
+    check();
+})
+
+c_password.addEventListener('click', () => {
+    removeClasses();
+})
+
+submit.addEventListener('click', () => {
+})
+
